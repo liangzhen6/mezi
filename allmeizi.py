9718f8d858fbe4333c8cbfc1967203aac06b362d
liangzhen6 <liangz@szmn.com.cn>
2017-06-03 16:41:00 +0800

    init
    111

--- allmeizi.py
+++ allmeizi.py
@@ -3,7 +3,14 @@
 from mongo_queue import MogoQueue
 from bs4 import BeautifulSoup
 from download import request
-spider_queue = MogoQueue('meinvxiezhenji', 'crawl_queue')
+import multiprocessing
+
+num_cpus = multiprocessing.cpu_count()
+spider_allqueue = []
+for x in range(num_cpus):
+	string = 'crawl_queue' + str(x)
+	spider_queue = MogoQueue('meinvxiezhenji', string)
+	spider_allqueue.append(spider_queue)
 
 def start(url):
 	response = request.get(url, 3)
@@ -11,9 +18,11 @@ def start(url):
 	all_div = Soup.find('div', class_ = 'all')
 	all_ul = all_div.find_all('ul', class_ = 'archives')
 	all_a = all_ul[0].find_all('a')
-	for a in all_a:
+	for index,a in enumerate(all_a):
 		title = a.get_text()
 		url = a['href']
+		spider_queue = spider_allqueue[index%4]
+		print(spider_queue)
 		spider_queue.push(url, title)
 
 
--- meizitu.py
+++ meizitu.py
@@ -12,8 +12,10 @@ SLEEP_TIME = 1
 global currentPath
 currentPath = os.path.abspath('.')
 
-def mztu_crawler(max_threads = 1):
-	crawl_queue = MogoQueue('meinvxiezhenji', 'crawl_queue') #这个是我们获取URL 的队列
+def mztu_crawler(name):
+	srting = 'crawl_queue' + name
+	crawl_queue = MogoQueue('meinvxiezhenji', srting) #这个是我们获取URL 的队列
+	
 	def pageurl_crawler():
 		while True:
 			try:
@@ -24,7 +26,7 @@ def mztu_crawler(max_threads = 1):
 				break
 			else:
 				img_urls = []
-				req = request.get(url, 3).text
+				req = request.get(url, 2).text
 				title = crawl_queue.pop_title(url)
 				mkdir(title)
 				max_span = BeautifulSoup(req, 'lxml').find('div', class_ = 'pagenavi').find_all('span')[-2].get_text()
@@ -33,9 +35,10 @@ def mztu_crawler(max_threads = 1):
 					main_image = BeautifulSoup(request.get(page_url, 3).text, 'lxml').find('div', class_='main-image')
 					print('sbbbb111',main_image)
 					img_url = main_image.find_all('img')[0]['src']
-					print('sbbbb222', img_url)
+					print('sbbbb222', srting, img_url)
 					img_urls.append(img_url)
 					save_img(img_url)
+					time.sleep(0.5)
 				crawl_queue.complete(url)
 				##img_queue.push_imgurl(title, img_urls)
                 ##print('插入数据库成功')
@@ -61,30 +64,33 @@ def mztu_crawler(max_threads = 1):
 			f.close()
 			print('保存完毕:%s' % (image_url))
 
+	pageurl_crawler()
 
-	threads = []
-	while threads or crawl_queue:
-		'''
-		这儿crawl_queue用上了，就是我们__bool__函数的作用，为真则代表我们MongoDB队列里面还有数据
-	        threads 或者 crawl_queue为真都代表我们还没下载完成，程序就会继续执行
-		'''
-		for thread in threads:
-			if not thread.is_alive():#is_alive 是判断是否为空，不是空队列则在队列中删掉
-				threads.remove(thread)
+
+	# threads = []
+	# while threads or crawl_queue:
+	# 	'''
+	# 	这儿crawl_queue用上了，就是我们__bool__函数的作用，为真则代表我们MongoDB队列里面还有数据
+	#         threads 或者 crawl_queue为真都代表我们还没下载完成，程序就会继续执行
+	# 	'''
+	# 	for thread in threads:
+	# 		if not thread.is_alive():#is_alive 是判断是否为空，不是空队列则在队列中删掉
+	# 			threads.remove(thread)
 	
-		while len(threads) < max_threads or crawl_queue.peek():###线程池中的线程少于max_threads 或者 crawl_qeue时
-			thread = threading.Thread(target = pageurl_crawler)#创建线程
-			thread.setDaemon(True) ##设置守护线程
-			thread.start() #启动线程
-			threads.append(thread)##添加进线程队列
-		time.sleep(SLEEP_TIME)
+	# 	while len(threads) < max_threads or crawl_queue.peek():###线程池中的线程少于max_threads 或者 crawl_qeue时
+	# 		thread = threading.Thread(target = pageurl_crawler)#创建线程
+	# 		thread.setDaemon(True) ##设置守护线程
+	# 		thread.start() #启动线程
+	# 		threads.append(thread)##添加进线程队列
+	# 	time.sleep(SLEEP_TIME)
 
 def process_crawler():
 	process = []
 	num_cpus = multiprocessing.cpu_count()
 	print('将启动进程数为：', num_cpus)
 	for i in range(num_cpus):
-		p = multiprocessing.Process(target = mztu_crawler) #创建进程
+		time.sleep(SLEEP_TIME)
+		p = multiprocessing.Process(target = mztu_crawler, args = [str(i),]) #创建进程
 		p.start() #启动进程
 		process.append(p) #添加进程队列
 	for p in process:
